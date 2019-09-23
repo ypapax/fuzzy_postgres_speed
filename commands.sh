75fc9601c8ae2194e03534b7eae41b4dc5490ddf
Maxim Yefremov <maxYefr@gmail.com>
2019-09-23 15:55:40 +0300

    slow: 4 minutes with letters

--- commands.sh
+++ commands.sh
@@ -8,8 +8,10 @@ run() {
 }
 
 rerun() {
+  set +e;
   docker kill fuzzy_postgres_speed_postgres_1
   docker rm fuzzy_postgres_speed_postgres_1
+  set -e;
   run
 }
 
@@ -31,7 +33,11 @@ last() {
 }
 
 fuz() {
-  sqla "SELECT * FROM person WHERE name % '1100011'"
+#  sqla "SELECT * FROM person WHERE name % '1100011'"
+  sqla "SELECT * FROM person WHERE name % '120000085000008500000RN3SGD19HYZXYEK9TM0WQW1W1F62PMI6ZDP5GH5M5VAUZKUIWL'"
+# Planning Time: 0.947 ms
+# Execution Time: 243752.916 ms
 }
+
 
 "$@"
--- init.sql
+++ init.sql
@@ -12,5 +12,8 @@ CREATE EXTENSION pg_trgm;
 CREATE INDEX name_trigram_idx ON person USING gin (name gin_trgm_ops);
 
 INSERT INTO person (name)
-SELECT CONCAT(g.name, g.name, g.name, g.name)
+SELECT CONCAT(g.name, g.name, g.name, (SELECT array_to_string(
+                                                      array(select substr('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+                                                                          ((random() * (36 - 1) + 1)::integer), 1)
+                                                            from generate_series(1, 50)), '')))
 FROM generate_series(1, 8500000) AS g (name);
