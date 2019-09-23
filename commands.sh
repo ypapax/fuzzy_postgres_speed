a2f1f44787d23f58aa6ef4f806fb898359ac962e
Maxim Yefremov <maxYefr@gmail.com>
2019-09-23 16:05:32 +0300

    letters with spaces: 1 second

--- commands.sh
+++ commands.sh
@@ -8,10 +8,10 @@ run() {
 }
 
 rerun() {
-  set +e;
+  set +e
   docker kill fuzzy_postgres_speed_postgres_1
   docker rm fuzzy_postgres_speed_postgres_1
-  set -e;
+  set -e
   run
 }
 
@@ -33,11 +33,12 @@ last() {
 }
 
 fuz() {
-#  sqla "SELECT * FROM person WHERE name % '1100011'"
-  sqla "SELECT * FROM person WHERE name % '120000085000008500000RN3SGD19HYZXYEK9TM0WQW1W1F62PMI6ZDP5GH5M5VAUZKUIWL'"
-# Planning Time: 0.947 ms
-# Execution Time: 243752.916 ms
-}
+  #  sqla "SELECT * FROM person WHERE name % '1100011'"
+  #  sqla "SELECT * FROM person WHERE name % '120000085000008500000RN3SGD19HYZXYEK9TM0WQW1W1F62PMI6ZDP5GH5M5VAUZKUIWL'"
+  # Planning Time: 0.947 ms
+  # Execution Time: 243752.916 ms
+  sqla "SELECT * FROM person WHERE name % '1200000'"
 
+}
 
 "$@"
--- init.sql
+++ init.sql
@@ -12,8 +12,10 @@ CREATE EXTENSION pg_trgm;
 CREATE INDEX name_trigram_idx ON person USING gin (name gin_trgm_ops);
 
 INSERT INTO person (name)
-SELECT CONCAT(g.name, g.name, g.name, (SELECT array_to_string(
-                                                      array(select substr('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-                                                                          ((random() * (36 - 1) + 1)::integer), 1)
-                                                            from generate_series(1, 50)), '')))
+SELECT CONCAT(g.name, ' ', g.name, ' ', g.name, ' ', (SELECT array_to_string(
+                                                                     array(select substr(
+                                                                                          'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+                                                                                          ((random() * (36 - 1) + 1)::integer),
+                                                                                          1)
+                                                                           from generate_series(1, 10)), '')))
 FROM generate_series(1, 8500000) AS g (name);
